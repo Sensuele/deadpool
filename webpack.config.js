acba3a03bb65831a94c9998c9d7b3d1edf50c686
Евгений Дружняев <druzhniaev@gmail.com>
2021-06-13 15:43:53 +0600

    Create project, add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './main.js',
+    // publicPath: './'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    open: true,
+  },
+  mode: 'development',
+
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+
+      {
+        test: /(\.scss|\.css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-sass-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: './postcss.config.js'
+              }
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: './fonts/',
+          publicPath: './fonts',
+        }
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: './img',
+            publicPath: './img',
+            useRelativePath: true
+          }
+        }]
+      },
+
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./main.css",
+    })
+  ]
+};
