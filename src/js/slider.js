a11b881fe9dd96310211b1458ebd1b1876602885
Евгений Дружняев <druzhniaev@gmail.com>
2021-06-14 11:51:33 +0600

    Styles for mobile

--- src/js/slider.js
+++ src/js/slider.js
@@ -10,23 +10,14 @@ let childSlider = new Swiper('.swiper-container', {
     nextEl: '.swiper-button-next',
   },
   breakpoints: {
-    992: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-      centeredSlides: false,
-      loop: true,
-      // Navigation arrows
-    
-    },
     720: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    560: {
       slidesPerView: 1,
       spaceBetween: 15,
-      navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next',
-      },
     },
-
   }
 });
 
